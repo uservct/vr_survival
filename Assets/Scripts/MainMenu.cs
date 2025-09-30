a4f9e873bf63e13f77137325c5d7e9bf8bbd61c3
Hisu04 <banbua75@gmail.com>
2025-09-30 14:59:01 +0700

    Map final

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // cần cho LoadScene
+
+public class MainMenu : MonoBehaviour
+{
+    // Gọi khi bấm nút New Game
+    public void NewGame()
+    {
+        SceneManager.LoadScene("MAP3"); // thay "GameScene" bằng tên scene chơi game của bạn
+    }
+
+    // Gọi khi bấm nút Quit
+    public void QuitGame()
+    {
+        Debug.Log("Thoát game..."); // để test trong Editor
+        Application.Quit(); // chỉ thoát khi build ra .exe/.apk
+    }
+}
